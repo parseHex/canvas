51ed51e7fbdf8c61bab4afe7fa267bb2c40d66c6
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2018-09-06 14:48:57 -0400

    Organize rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,34 +2,40 @@ import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const tsConfig = {
 	useTsconfigDeclarationDir: true,
 };
+const sourcemap = !isProd;
+const plugins = [
+	resolve(),
+	typescript(tsConfig),
+];
 
-export default [
-	{
-		input: 'src/index.ts',
-		output: {
-			name: 'protoCanvas',
-			file: pkg.browser,
-			format: 'umd',
-			sourcemap: true,
-		},
-		plugins: [
-			resolve(),
-			typescript(tsConfig),
-		],
-	},
+const outputs = [
 	{
 		input: 'src/index.ts',
 		output: {
 			file: pkg.module,
 			format: 'es',
-			sourcemap: true,
+			sourcemap,
 		},
-		plugins: [
-			resolve(),
-			typescript(tsConfig),
-		],
+		plugins,
 	},
 ];
+
+if (isProd) {
+	outputs.push({
+		input: 'src/index.ts',
+		output: {
+			name: 'protoCanvas',
+			file: pkg.browser,
+			format: 'umd',
+			sourcemap,
+		},
+		plugins,
+	});
+}
+
+export default outputs;
