970fcbfa73e00e0a8d55208a449ccdf2f95e12bd
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2018-09-08 17:36:55 -0400

    remove object ifc

--- src/ifc/object/position.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace ObjectPosition {
-	export type Rectangle = Coordinate;
-	export type Ellipse = Coordinate;
-	export type Point = Coordinate;
-	export type Text = Coordinate;
-	export type Line = [Coordinate, Coordinate];
-	export type Triangle = [Coordinate, Coordinate, Coordinate];
-	export type Polygon = Coordinate[];
-}
