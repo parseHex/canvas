5cf172a441359cca9dda649635ef8791e32881b2
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2018-09-08 17:15:54 -0400

    clear utilities

--- src/utility.ts
+++ src/utility.ts
@@ -1,64 +1,6 @@
-import state from './state';
-
 /**
- * Clear the canvas.
+ * Make a clone of some data by `stringify`-ing and then `parse`-ing it.
  */
-export function clear() {
-	state.ctx.clearRect(0, 0, state.canvas.width, state.canvas.height);
-}
-
-/**
- * Set the canvas's transform to its default values.
- */
-export function resetTransform() {
-	state.ctx.setTransform(1, 0, 0, 1, 0, 0);
-}
-
-/**
- * Add an onClick handler to the canvas.
- */
-export function onClick(handler: MouseEventHandler) {
-	state.canvas.addEventListener('click', (event: MouseEvent) => {
-		const mousePos = {
-			x: event.offsetX,
-			y: event.offsetY,
-		};
-
-		handler(mousePos);
-	});
-}
-
-/**
- * Add an onDrag handler to the canvas.
- *
- * `handler` will also be called once on `mousedown` event.
- */
-export function onDrag(handler: MouseEventHandler) {
-	state.canvas.addEventListener('mousedown', (event: MouseEvent) => {
-		state.dragging = true;
-
-		const mousePos = {
-			x: event.offsetX,
-			y: event.offsetY,
-		};
-
-		handler(mousePos);
-	});
-
-	// listen for mouseup on window to catch the event
-	// when not hovering over canvas
-	window.addEventListener('mouseup', () => {
-		state.dragging = false;
-	});
-
-	state.canvas.addEventListener('mousemove', (event: MouseEvent) => {
-		if (!state.dragging) return;
-
-		const mousePos = {
-			x: event.offsetX,
-			y: event.offsetY,
-		};
-
-		handler(mousePos);
-	});
+export function clone<T>(data: T): T {
+	return JSON.parse(JSON.stringify(data));
 }
