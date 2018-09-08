c2862009f7c236c2158e6458418d695fa31e6ea6
Thomas Mays <thomasmays@Thomass-MacBook-Pro.local>
2018-09-08 18:36:16 -0400

    remove state

--- src/ifc/state.d.ts
+++
@@ -1,8 +0,0 @@
-interface State {
-	canvas: HTMLCanvasElement;
-	ctx: CanvasRenderingContext2D;
-	backgroundCanvas: HTMLCanvasElement;
-	backgroundCtx: CanvasRenderingContext2D;
-	dragging: boolean;
-	objects: import('../object/shapes/_base').default[];
-}
